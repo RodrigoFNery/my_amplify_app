6c05ed9466f4c872e23113bda48dff8edaf884d6
rodrigofnery <rodrigofnery@gmail.com>
2024-09-10 22:12:58 -0300

    Cognito Sign In working

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, View, StyleSheet, SafeAreaView } from "react-native";
 
-export default function App() {
+import { Amplify } from "aws-amplify";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+
+import outputs from "./amplify_outputs.json";
+
+// Amplify.configure(outputs);
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolClientId: '25r3m05corkq402vi0kbcoto5j',
+      userPoolId: 'us-east-1_tzT9Dvnz9',
+      }
+    }
+});
+
+const SignOutButton = () => {
+  const { signOut } = useAuthenticator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.signOutButton}>
+      <Button title="Sign Out" onPress={signOut} />
     </View>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Authenticator.Provider>
+      <Authenticator>
+        <SafeAreaView>
+          <SignOutButton />
+        </SafeAreaView>
+      </Authenticator>
+    </Authenticator.Provider>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  signOutButton: {
+    alignSelf: "flex-end",
   },
 });
+
+export default App;
